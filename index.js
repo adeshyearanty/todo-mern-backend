6311bade8622109672e4062cee75f04136c9bdd3
Adesh Yearanty <adesh.yearanty@gmail.com>
2025-02-27 11:56:11 +0530

    configured the files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
 import connectDB from './db/db-conn.js';
+import { PORT } from './config/config.js';
 
 const app = express();
 app.use(cors());
@@ -11,7 +12,7 @@ app.use(router);
 function startServer() {
   try {
     connectDB();
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server is listening on port 3000");
     });
   } catch (error) {
